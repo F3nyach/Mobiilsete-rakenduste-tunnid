9ac259e75cba42ddab9474c1293330b4455e7f7e
Andrei Kiseljov <andrei.kiseljov@tptlive.ee>
2019-11-30 20:48:10 +0200

    made a simple list of 3 ppl

--- Kodutoo_3_ListView/Kodutoo_3_ListView/MainActivity.cs
+++ Kodutoo_3_ListView/Kodutoo_3_ListView/MainActivity.cs
@@ -3,18 +3,31 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System.Collections.Generic;
 
 namespace Kodutoo_3_ListView
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private List<string> userItems;
+        private ListView snListView;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            snListView = FindViewById<ListView>(Resource.Id.snListView); // sn - social network :P
+            userItems = new List<string>();
+            userItems.Add("Elon Musk");
+            userItems.Add("Everyone on the internet");
+            userItems.Add("Just some random dude");
+
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, userItems);
+
+            snListView.Adapter = adapter;
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
